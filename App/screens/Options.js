58b22e15220a3ae7c3be098acfc02f5c35e46876
Pankaj Singh <resopankaj@gmail.com>
2020-09-30 12:28:41 +0200

    add RowItem component

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,54 +1,37 @@
 import React from "react";
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-  SafeAreaView,
-} from "react-native";
+import { SafeAreaView, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
 import colors from "../constants/colors";
+import { RowItem, RowSeparator } from "../components/RowItem";
 
-const styles = StyleSheet.create({
-  row: {
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    backgroundColor: colors.white,
-  },
-  title: {
-    color: colors.text,
-    fontSize: 16,
-  },
-  separator: {
-    backgroundColor: colors.border,
-    height: StyleSheet.hairlineWidth,
-    marginLeft: 20,
-  },
-});
-
-const Options = () => {
+export default () => {
   return (
-    <SafeAreaView>
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>Themes</Text>
-      </TouchableOpacity>
+    <View>
+      <RowItem />
+      <RowItem
+        title="Themes"
+        onPress={() => alert("todo!")}
+        rightIcon={
+          <Entypo name="chevron-right" size={20} color={colors.blue} />
+        }
+      />
 
-      <View style={styles.separator} />
+      <RowSeparator />
 
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>React Native Basics</Text>
-      </TouchableOpacity>
+      <RowItem
+        title="React Native Basics"
+        onPress={() => alert("todo!")}
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+      />
 
-      <View style={styles.separator} />
+      <RowSeparator />
 
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>React Native by Example</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+      <RowItem
+        title="React Native by Example"
+        onPress={() => alert("todo!")}
+        rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+      />
+    </View>
   );
 };
-
-export default Options;
