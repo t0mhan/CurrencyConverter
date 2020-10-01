5e40269a8e22683c5837ff62391f4ac8868ba65f
Pankaj Singh <resopankaj@gmail.com>
2020-10-01 10:54:46 +0200

    added navigation

--- App/index.js
+++ App/index.js
@@ -3,6 +3,12 @@ import React from "react";
 
 // app
 import Home from "./screens/Home";
+import Navigation from "./config/Navigation";
 
+const navigation = () => <Navigation />;
+export default navigation;
+
+/*
 const Index = () => <Home />;
 export default Index;
+*/
--- App/screens/Options.js
+++ App/screens/Options.js
@@ -1,26 +1,30 @@
-import React from "react";
-import { Alert, SafeAreaView, ScrollView, Linking } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Linking,
+  Alert,
+  StatusBar,
+} from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
-import colors from "../constants/colors";
-import { RowItem, RowSeparator } from "../components/RowItem";
+import colors from '../constants/colors';
+import { RowItem, RowSeparator } from '../components/RowItem';
 
-const openURL = (url) => {
-  return Linking.openURL(url).catch(() => {
-    Alert.alert(
-      "Sorry something went wrong",
-      "Please check the url and try again!"
-    );
-  });
-};
+const openLink = (url) =>
+  Linking.openURL(url).catch(() =>
+    Alert.alert('Sorry, something went wrong.', 'Please try again later.')
+  );
+
 export default () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.white} />
+
       <ScrollView>
-        <RowItem />
         <RowItem
-          title="Learn React Native"
-          onPress={() => openURL("https://reactnative.dev/docs/tutorial")}
+          title="Themes"
+          onPress={() => alert('todo!')}
           rightIcon={
             <Entypo name="chevron-right" size={20} color={colors.blue} />
           }
@@ -30,7 +34,11 @@ export default () => {
 
         <RowItem
           title="React Native Basics"
-          onPress={() => openURL("https://reactnative.dev/docs/components-and-apis")}
+          onPress={() =>
+            openLink(
+              'https://learn.reactnativeschool.com/p/react-native-basics-build-a-currency-converter'
+            )
+          }
           rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
         />
 
@@ -38,7 +46,7 @@ export default () => {
 
         <RowItem
           title="React Native by Example"
-          onPress={() => alert("todo!")}
+          onPress={() => openLink('https://reactnativebyexample.com')}
           rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
         />
       </ScrollView>
